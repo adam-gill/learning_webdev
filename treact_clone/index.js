a288a1c7228a7229d5d779443c8f38642a560843
adam-gill <adam.douglas.gill@gmail.com>
2023-08-31 22:35:02 -0400

    worked on more scalability

--- treact_clone/index.js
+++ treact_clone/index.js
@@ -5,3 +5,14 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+
+
+// const input = document.getElementById('input');
+// const container = document.getElementById('input__container');
+
+// function handleClick() {
+//     input__container.classList.add('email__input-box-active');
+// }
+
+
+// input.addEventListener('click', handleClick());
